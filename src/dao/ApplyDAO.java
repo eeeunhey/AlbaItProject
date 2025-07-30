aa5a87c3ab12b5823e58ba4e247f1fad14a8fb22
eunhye <dmsgp2627@naver.com>
2025-07-30 23:18:17 +0900

    ApplyDAO Update

--- src/dao/ApplyDAO.java
+++ src/dao/ApplyDAO.java
@@ -1,5 +1,12 @@
 package dao;
 
+import java.util.List;
+
 public class ApplyDAO {
+	
+	public List<String[]> getMyApplies(String loginUserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/personalBoardUI/PersonalApplyJob.java
+++ src/personalBoardUI/PersonalApplyJob.java
@@ -1,12 +1,21 @@
 package personalBoardUI;
+import java.util.List;
 
+import dao.ApplyDAO;
 import ui.BaseUI;
 
 public class PersonalApplyJob extends BaseUI {
 
 	@Override
 	public void execute() throws Exception {
-		
+		  ApplyDAO dao = new ApplyDAO();
+		    List<String[]> list = dao.getMyApplies(BaseUI.loginUserId);
+
+		    System.out.println("지원현황");
+		    System.out.println("번호 | 공고번호 | 제목 | 상태 | 지원일");
+		    for (String[] row : list) {
+		        System.out.printf("%s | %s | %s | %s | %s\n",
+		            row[0], row[1], row[2], row[3], row[4]);
 		
 	}
 }
